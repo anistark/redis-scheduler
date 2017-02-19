1c6748920dc75d29eedc8c41d7bda3e955661533
Kumar Anirudha <anirudhastark@yahoo.com>
2017-02-19 23:40:56 +0530

    redis scheduler package

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# scheduler
-Scheduler
+# Schedule
+
+
+
---
+++ redis-scheduler/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'anistark'
+__version__ = '0.0.1'
+__title__ = 'RedisScheduler'
+
+# Importing Classes
+# from redis-scheduler import RedisScheduler
